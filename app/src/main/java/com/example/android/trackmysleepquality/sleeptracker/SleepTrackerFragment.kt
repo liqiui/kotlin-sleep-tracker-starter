cd59c506648e6609add99eabd7afe6446339a120
liqiuli <liqiui@outlook.com>
2020-04-28 20:18:35 +1000

    transformation maps ,recording sleep Quality ,button states

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -22,7 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -47,7 +49,7 @@ class SleepTrackerFragment : Fragment() {
                 inflater, R.layout.fragment_sleep_tracker, container, false)
 
        val application = requireNotNull(this.activity).application
-        val dataSource = SleepDatabase.getInstane(application).sleepDatabaseDao
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
         val viewModelFactory = SleepTrackerViewModelFactory(dataSource,application)
         val sleepTrackerViewModel =
                 ViewModelProviders.of(
@@ -55,6 +57,16 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
         binding.setLifecycleOwner (this)
+        sleepTrackerViewModel.navigateToSleepQuality.observe(this, Observer { night ->
+            night?.let {
+                this.findNavController().navigate(
+                        SleepTrackerFragmentDirections
+                                .actionSleepTrackerFragmentToSleepQualityFragment(night.nightId))
+
+                sleepTrackerViewModel.doneNavigating()
+            }
+
+        })
 
         return binding.root
     }
--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,6 +18,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -49,6 +50,14 @@ class SleepTrackerViewModel(
         formatNights(nights, application.resources)
     }
 
+    private val __navigateToSleepQuality = MutableLiveData<SleepNight>()
+
+    val navigateToSleepQuality : LiveData<SleepNight>
+        get() = __navigateToSleepQuality
+
+    fun doneNavigating(){
+        __navigateToSleepQuality.value = null
+    }
 
     init {
         initializeTonight()
@@ -94,6 +103,7 @@ class SleepTrackerViewModel(
             oldNight.endTimeMilli = System.currentTimeMillis()
 
             update(oldNight)
+            __navigateToSleepQuality.value = oldNight
         }
     }
 
